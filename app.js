afca326cbfea08c58972f55699cfe76af4f363b8
Abhay <Abhaysengar3250@gmail.com>
2021-11-20 11:06:13 +0530

    Final Version 3.0

--- app.js
+++ app.js
@@ -1,21 +1,19 @@
 
-
-
-
- var matrix = [[]];
-
+var matrix = [[]];   //Variables declaration
 var waste = [];
-
-var discarded = [];
+var discarded = []; //Arrays
 var used =[];
 var uneaten = [];
-
 var t=0;
 var tDis= 0;
-var tused = 0;
+var tused = 0;  //Ints
 var tunused = 0;
+var allItems =[];
 
-function listToMatrix(list, elementsPerSubArray) {
+
+
+
+function listToMatrix(list, elementsPerSubArray) { // Function for converting array to double array
     var matrix1 = [], i, k;
 
     for (i = 0, k = -1; i < list.length; i++) {
@@ -32,9 +30,13 @@ function listToMatrix(list, elementsPerSubArray) {
 }
 
   
-function push1() {
-    $(".date").each(function(){
+function push1() {    //  pushing values obtained from web to arrays using jquery
+    $(".count").each(function(){
         waste.push($(this).val());
+        
+       });
+       $(".input1").each(function(){
+        allItems.push($(this).val());
         
        });
        $(".text1").each(function(){
@@ -53,40 +55,31 @@ function push1() {
         
        });
 
-     matrix = listToMatrix(waste, 4);
+
+
+     matrix = listToMatrix(allItems, 4);  // array to double array with 4 as a elements per array
      
        
-    
 }
 
 
 
-    
 
-
-
-    
-
-
-
-
-
-
-$(".xl1").click(function(){
-    var wb = XLSX.utils.book_new();
-wb.Props = {
+$(".xl1").click(function(){  // adding event listener to xl1 button 
+    var wb = XLSX.utils.book_new(); // Creating new xl workspace 
+wb.Props = {   // storing details of workspace in document
     Title: "SheetJS Tutorial",
-    Subject: "Test",
-    Author: "Red Stapler",
+    Subject: "Inventory",
+    Author: "FOODBUCKS",
     CreatedDate: new Date()
 };
-wb.SheetNames.push("Food Inventory");
+wb.SheetNames.push("Food Inventory"); // sheet name
 
 var ws_data = matrix; 
 var ws = XLSX.utils.aoa_to_sheet(ws_data); 
 wb.Sheets["Food Inventory"] = ws;
 
-var wbout = XLSX.write(wb, {bookType:'xlsx',  type: 'binary'});
+var wbout = XLSX.write(wb, {bookType:'xlsx',  type: 'binary'}); // writing doc in excel 
 function s2ab(s) { 
     var buf = new ArrayBuffer(s.length); //convert s to arrayBuffer
     var view = new Uint8Array(buf);  //create uint8array as viewer
@@ -95,21 +88,18 @@ function s2ab(s) {
 }
 
 
-    saveAs(new Blob([s2ab(wbout)],{type:"application/octet-stream"}), 'test.xlsx');
-    console.log(matrix);
+    saveAs(new Blob([s2ab(wbout)],{type:"application/octet-stream"}), 'test.xlsx'); // Saving the sheet with new information
     
     
 });
 
 
  
-$(".submit").click(function name(params) {
+$(".submit").click(function name(params) { //  
     push1();
     console.log(uneaten);
-
-
-
-    waste.forEach( function (items) {
+    
+    waste.forEach( function (items) { // getting information from array and computing them.
         t+= Number(items);
         waste =[];
    
@@ -129,16 +119,16 @@ $(".submit").click(function name(params) {
        uneaten = [];
       
    });
-
-            $(".date").val("");
-            alert("Items added to database");
+ 
+    $(".date").val("");    // Clearing up the input fields
+    alert("Items added to database"); // code for alret dialog box
    
     
-    $(".unused").text(tunused);
+    $(".unused").text(tunused);  // sending info back to page
     $(".used").text(tused);
     $(".dis").text(tDis);
     $(".waste1").text(t-tused); 
-    var element = document.getElementById("div3");
+    var element = document.getElementById("div3"); // Adding CSS class to div3 div
         element.classList.add("summary");
 });
 
@@ -153,9 +143,26 @@ $(".submit").click(function name(params) {
     //  waste.forEach( function (items) {
     //      t+= Number(items);
         // console.log(waste);
+
+        
         
          
-     
+        // function totals( arrayname , tname ) {
+        //     arrayname.forEach( function (items) { // getting information from array and computing them.
+        //         tname+= Number(items);
+        //         arrayname =[];
+           
+        //     });
+        
+    
+            
+        // }
+    
+        // totals(waste , t);
+        // console.log(t);
+        // totals(used , tused);
+        // totals(discarded , tDis);
+        // totals(uneaten , tuneaten);
 
 
 
